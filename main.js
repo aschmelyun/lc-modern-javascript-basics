4b0ade7cb3b8d3304772ab392437450d60c16e38
Andrew Schmelyun <schmelyun@gmail.com>
2022-02-24 06:22:36 -0500

    Episode 9: Demystifying Modules

--- main.js
+++ main.js
@@ -1,46 +1,9 @@
 'use strict';
 
-let fruit = ['Banana', 'Strawberry', 'Orange', 'Apple', 'Grape', 'Plum'];
+import { users, currentUser } from './users.js';
+import helper from './helper.js';
 
-// let fruitsThatContainE = [];
-// fruit.forEach(function(fruit) {
-//     if (fruit.includes('e')) {
-//         fruitsThatContainE.push(fruit);
-//     }
-// });
+// document.getElementById('output').innerHTML = users[currentUser].name;
 
-// filter example
-
-let fruitsThatContainE = fruit.filter(f => f.includes('e'));
-
-document.getElementById('output').innerHTML = fruitsThatContainE.join(', ');
-
-// reduce example
-
-let users = [
-    {
-        name: 'Andrew',
-        email: 'andrew@example.com',
-        stars: 5,
-    },
-    {
-        name: 'Ashley',
-        email: 'ashley@example.com',
-        stars: 9
-    },
-    {
-        name: 'Gaston',
-        email: 'gaston@example.com',
-        stars: 1
-    }
-];
-
-let totalStars = users.reduce((stars, user) => stars += user.stars, 0);
-
-document.getElementById('output').innerHTML += '<br>Total Stars: ' + totalStars;
-
-// map example
-
-let userEmails = users.map(user => user.email);
-
-document.getElementById('output').innerHTML += '<br>Emails: ' + userEmails.join(', ');
+const userEmails = users.map(user => user.email);
+document.getElementById('output').innerHTML = helper(userEmails).join(', ');
