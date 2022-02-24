02f44b727d1a61c3e2317eb0da35661630616808
Andrew Schmelyun <schmelyun@gmail.com>
2022-02-24 05:52:07 -0500

    Episode 5: Promises Are Made

--- main.js
+++ main.js
@@ -1,72 +1,38 @@
 'use strict';
 
-// // Initialize an Item class
-// function Item(name, category) {
-//     this.name = name;
-//     this.category = category;
-// }
+let myPromise = new Promise((resolve, reject) => {
+    // call an api
+    let user = {
+        name: 'Andrew',
+        email: 'andrew@example.com'
+    };
 
-// // Create a method called getDetails() for an Item
-// Item.prototype.getDetails = function() {
-//     return `${this.name} - ${this.category}`;
-// }
+    setTimeout(() => {
+        // resolve(user);
+        reject('Sorry, could not retrieve the user.');
+    }, 2000);
+});
 
-// // Initialize a PurchasedItem class, inherits the Item class
-// function PurchasedItem(name, category, price) {
-//     Item.call(this, name, category);
-//     this.price = price;
-// }
+const getAdditionalUserData = user => {
+    document.getElementById('output').innerHTML = `${user.name} (${user.email})`;
 
-// // Sets the inherited methods and properties of the base class
-// PurchasedItem.prototype = Object.create(Item.prototype);
-// PurchasedItem.prototype.constructor = PurchasedItem;
+    return new Promise((resolve, reject) => {
+        // calling another api to get more user data
+        user.favoriteColor = 'Blue';
+        user.currentDrink = 'La Croix';
 
-// // Creates a new method just for the extended PurchasedItem class
-// PurchasedItem.prototype.getDetailsWithPrice = function() {
-//     return `${this.name} - ${this.category} - $${this.price}`;
-// }
-
-// var item = new Item('Coffee', 'Food');
-// item.category = 'Drinks';
-
-// var purchasedItem = new PurchasedItem('Sugar', 'Food', '2.49');
-
-class Item {
-    constructor(name, category) {
-        this.name = name;
-        this.category = category;
-    }
-
-    static maxItems = 10;
-
-    static getHelperText() {
-        return 'Add some items to your grocery list';
-    }
-
-    getDetails() {
-        return `${this.name} - ${this.category}`;
-    }
+        setTimeout(() => {
+            resolve(user);
+        }, 2000);
+    });
 }
 
-class PurchasedItem extends Item {
-    constructor(name, category, price) {
-        super(name, category);
-        this.price = price;
-    }
+myPromise.then(getAdditionalUserData)
+    .then((user) => {
+        document.getElementById('output').innerHTML = `${user.name} (${user.email}) - ${user.currentDrink}`;
+    })
+    .catch((error) => {
+        document.getElementById('output').innerHTML = error;
+    });
 
-    getDetailsWithPrice() {
-        return `${this.name} - ${this.category} - $${this.price}`;
-    }
-
-    static getNumberOfItems() {
-        return `3 / ${super.maxItems}`;
-    }
-}
-
-// let item = new Item('Coffee', 'Food');
-// item.category = 'Drinks';
-
-// let purchasedItem = new PurchasedItem('Sugar', 'Food', '2.49');
-
-// document.getElementById('output').innerHTML = item.getDetails();
-document.getElementById('output').innerHTML = PurchasedItem.getNumberOfItems();
+document.getElementById('output').innerHTML = 'Look ma, no blocking!';
